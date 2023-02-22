7688391627262a5cbc1905b019c3ed9d1d40ae7d
Pratusha Pandey <pratushapandey08@gmail.com>
2023-02-22 21:42:56 +0530

    Create Selection Sort.py

---
+++ Sorting/Selection Sort.py
@@ -0,0 +1,19 @@
+# Selection Sort
+
+def selection_sort(arr):
+    for i in range(len(arr)-1):
+        min_index = i
+        for j in range(i+1,len(arr)):
+            if arr[min_index] > arr[j]:
+                min_index = j
+        
+        arr[i],arr[min_index] = arr[min_index],arr[i]
+
+arr = [72,50,10,44,8,20]
+print("Before selection sort:")
+print(arr)
+print(len(arr))
+
+insertion_sort(arr)
+print("After selection sort:")
+print(arr)
